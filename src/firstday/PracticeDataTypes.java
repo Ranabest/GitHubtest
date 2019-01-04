240549ca7f023ecfb06f886a41dff378ce37f4b9
mkari <mkarim20@live.com>
2019-01-04 16:27:46 -0500

    mm

---
+++ src/firstday/PracticeDataTypes.java
@@ -0,0 +1,26 @@
+package firstday;
+
+public class PracticeDataTypes {
+
+	public static void main(String[] args) {
+		
+		int a=5;
+		System.out.println("My integer number is: " +a);
+		byte b=120;
+		System.out.println("My byte number is: " +b);
+		short c = 12000;
+		System.out.println("My short number is: " +c);
+		long d = 120000000;
+		System.out.println("My long number is: " +d);
+		boolean e = true;
+		System.out.println("My boolean is: " +e);
+		char f = 'r';
+		System.out.println("My char is: " +f);
+		float g = 12.22f;
+		System.out.println("My float is: " +g);
+		double h = 99.99;
+		System.out.println("My double is: " +h);
+
+	}
+
+}
